1af333804b708e88d30815b9bf409fdce84a0b9e
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-11-15 12:04:16 +0530

    sending and responding request learned

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
-console.dir(app); 
+// console.dir(app); 
 let port = 3000;
-app.listen(port, () => {
+app.listen(port, () => {/**listen make the webserver for the incomming api*/
     console.log(`app is listing on port ${port}`);
     
 
 });
 // this is the main index File for node. 
+
+/**app.use((req, res)=> {
+    console.log("request recived");
+});*/ /**this will recived the request */
+
+
+// -----------------------------------------------------------------------------------
+
+app.use((req, res)=> {
+    console.log("request recived");
+    res.send({
+        name: "Nikhil Vaishnav",
+        college: "GITS"
+    });
+});/**this will send the responose to server */
+
+/**note - the express will convert the text format to json fromat */
