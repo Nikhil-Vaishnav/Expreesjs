6361fc58af7860b0bfb36d33a6290093f055930c
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-11-19 08:28:26 +0530

    used nodemon for auto server

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const app = express();
-// console.dir(app); 
-let port = 8080;
-app.listen(port, () => {/**listen make the webserver for the incomming api*/
-    console.log(`app is listing on port ${port}`);
+// const express = require("express");
+// const app = express();
+// // console.dir(app); 
+// let port = 8080;
+// app.listen(port, () => {/**listen make the webserver for the incomming api*/
+//     console.log(`app is listing on port ${port}`);
     
 
-});
+// });
 // this is the main index File for node. 
 
 /**app.use((req, res)=> {
@@ -29,32 +29,73 @@ app.listen(port, () => {/**listen make the webserver for the incomming api*/
 
 /**now we will use the get on place of the use for making differnet routes */
 
-app.get("/", (req, res) =>{
-    res.send("you contacted root path");
-});
+// app.get("/", (req, res) =>{
+//     res.send("you contacted root path");
+// });
 
-app.get("/", (req, res) =>{
-    res.send("you contacted root path");
-});
+// app.get("/", (req, res) =>{
+//     res.send("you contacted root path");
+// });
 
-app.get("/help", (req, res) =>{
-    res.send("you contacted help path");
-});
+// app.get("/help", (req, res) =>{
+//     res.send("you contacted help path");
+// });
 
-app.get("/search", (req, res) =>{
-    res.send("you contacted search path");
-});
+// app.get("/search", (req, res) =>{
+//     res.send("you contacted search path");
+// });
 
-app.get("/newPage", (req, res) =>{
-    res.send("you contacted newPage path");
-});
+// app.get("/newPage", (req, res) =>{
+//     res.send("you contacted newPage path");
+// });
 
-app.get("*", (req, res) =>{
-    res.send("This page does not available");
-});/**this will print when ther is search is out for all requests */
+// app.get("*", (req, res) =>{
+//     res.send("This page does not available");
+// });/**this will print when ther is search is out for all requests */
 
-app.post("/", (req, res) =>{
-    res.send("you sent a post request");
-});/**this a post method */
+// app.post("/", (req, res) =>{
+//     res.send("you sent a post request");
+// });/**this a post method */
 // -----------------------------------------------------------------------------------
- 
+
+
+// Path paremeters
+
+
+const express = require("express");
+const app = express();
+// console.dir(app); 
+let port = 8080;
+app.listen(port, () => {/**listen make the webserver for the incomming api*/
+    console.log(`app is listing on port ${port}`);
+});
+
+// app.get("/:username", (req, res) =>{
+//     console.log(req.params);
+//     res.send("you contacted newPage path");
+// });
+
+            // ----------------------------
+                    //    OR 
+            // ----------------------------  
+            
+            // app.get("/:username/:id", (req, res) =>{
+            //     console.log(req.params);
+            //     res.send("you contacted newPage path");
+            // });           
+            // ----------------------------  
+
+            // app.get("/:username/:id", (req, res) =>{
+            //     let {username, id} = req.params;
+            //     console.log(req.params);
+            //     res.send(`welcome to the page of insta @${username}`);
+            // });        
+            
+            // ----------------------------  
+                // we can also send html string 
+            app.get("/:username/:id", (req, res) =>{
+                let {username, id} = req.params;
+                console.log(req.params);
+                let htmlStr =   `<h1>welcome to the page of insta @${username}${id}<h1>`
+            res.send(htmlStr);
+            });           
