ebe49622154b480924dc64705310392d03e9356c
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-11-25 15:15:41 +0530

    learned about the qurey selector and with this day 31 completed

--- index.js
+++ index.js
@@ -62,13 +62,13 @@
 // Path paremeters
 
 
-const express = require("express");
-const app = express();
-// console.dir(app); 
-let port = 8080;
-app.listen(port, () => {/**listen make the webserver for the incomming api*/
-    console.log(`app is listing on port ${port}`);
-});
+// const express = require("express");
+// const app = express();
+// // console.dir(app); 
+// let port = 8080;
+// app.listen(port, () => {/**listen make the webserver for the incomming api*/
+//     console.log(`app is listing on port ${port}`);
+// });
 
 // app.get("/:username", (req, res) =>{
 //     console.log(req.params);
@@ -92,12 +92,34 @@ app.listen(port, () => {/**listen make the webserver for the incomming api*/
             // });        
             
             // ----------------------------  
-                // we can also send html string 
-            app.get("/:username/:id", (req, res) =>{
-                let {username, id} = req.params;
-                console.log(req.params);
-                let htmlStr =   `<h1>welcome to the page of insta @${username}${id}<h1>`
-            res.send(htmlStr);
-            });           
-            // ---------------------------- 
+            //     // we can also send html string 
+            // app.get("/:username/:id", (req, res) =>{
+            //     let {username, id} = req.params;
+            //     console.log(req.params);
+            //     let htmlStr =   `<h1>welcome to the page of insta @${username}${id}<h1>`
+            // res.send(htmlStr);
+            // });           
+            // // ---------------------------- 
             
+// //  query string
+            // app.get("/search",(req,res)=>{
+            //     console.log(req.query);/**this is qurey storing perameter */
+            //     res.send("no result");
+            // });
+
+//             // http://localhost:8080/search?q="Nikhil"
+//             // http://localhost:8080/search?q=Nikhil&Vaishnav
+
+// to extract the query 
+
+// app.get("/search",(req,res)=>{
+//     let { q } = req.query;
+//     if(!q){
+//         res.send("<h1>nothing searched</h1>")
+//     }
+//     res.send(`<h1>search result for query: ${q}</h1>`);
+// });
+
+// -------------------------------------------------------------------------------
+                            //  END
+// -------------------------------------------------------------------------------
